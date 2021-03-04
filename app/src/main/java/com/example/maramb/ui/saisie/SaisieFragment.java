e40119152f377d5648b4216ee02837aed4995e3b
boulinator <langloisnathan@gmail.com>
2021-03-04 19:28:04 +0100

    Take picture

--- app/src/main/java/com/example/maramb/ui/saisie/SaisieFragment.java
+++ app/src/main/java/com/example/maramb/ui/saisie/SaisieFragment.java
@@ -1,9 +1,14 @@
 package com.example.maramb.ui.saisie;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +18,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.maramb.R;
+import com.example.maramb.ui.ra.RaViewModel;
 
 public class SaisieFragment extends Fragment {
     private SaisieViewModel saisieViewModel;
@@ -29,6 +35,20 @@ public class SaisieFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
         return root;
+    };
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }
+        });
     }
 }
