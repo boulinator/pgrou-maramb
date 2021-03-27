0049e94ff06f75c05b4336beefb8c2acceaf2f9e
Samsoum41 <hocine.samy.sh@gmail.com>
2021-03-27 14:38:16 +0100

    Sont à présent pris en compte : - Le cas où l'utilisateur se trompe. - La création de compte
    Des améliorations ont été apportée aux classes ConnexionActivity et CreerCompteActivity :
    - Appels en base de donnée de manière asynchrone avec des AsyncTask
    - Des vérifications plus contraignantes sur les données d'entrée
    - Un cryptage des mots de passes avec l'algorithme BCrypt. Pour cela, j'ai utilisé la librairie open source JBCrypt. Il a fallu ajouter aux dépendances un fichier, mais en les rajoutant directement dans les dossiers du projet et non dans le build gradle.
    - Ajout des champs mail et confirmation de mot de passe pour la création de compte.
    
    On pourrait envisager de :
    - exiger un mot de passe plus complexe.
    - permettre la connexion avec son adresse mail.
    - ajouter une fonctionnalité mot de passe oublié.

--- app/src/main/java/com/example/maramb/ConnexionActivity.java
+++ app/src/main/java/com/example/maramb/ConnexionActivity.java
@@ -3,13 +3,20 @@ package com.example.maramb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import org.mindrot.jbcrypt.BCrypt;
+
 import java.sql.*;
 
+/**
+ * Classe représentant l'activité de la page de connexion. Elle comprend les mécanismes d'authentification et implémente les redirections vers d'autres pages.
+ */
 public class ConnexionActivity extends AppCompatActivity {
 
     @Override
@@ -18,53 +25,105 @@ public class ConnexionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_connexion);
     }
 
+    /**
+     * Méthode appelé par l'action du bouton de création de compte. Elle redirige simplement vers la page(activité) de création de compte.
+     * @param view  : Widget actionnant la méthode, ici ce sera le bouton de création de compte normalement.
+     */
     public void CreerCompte(View view) {
         Intent intent = new Intent(this, CreerCompteActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Méthode appelé par l'action du bouton de connexion. Elle sert à authentifier l'utilisateur auprès de la base de donnée PostGIS directement.
+     * @param view  : Widget actionnant la méthode, ici ce sera le bouton de connexion normalement.
+     */
     public void Connexion(View view) {
-        EditText conn_id = (EditText) findViewById(R.id.connexion_nomUtilisateur);
-        EditText conn_mdp = (EditText) findViewById(R.id.connexion_mdp);
-        String nomUtilisateur = conn_id.getText().toString();
-        String mdp = conn_mdp.getText().toString();
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        try{
-            Class.forName("org.postgresql.Driver");
-            System.out.println("Driver OK");
-            String url = "jdbc:postgresql://ser-info-03.ec-nantes.fr:5432/maramb";
-            String user = "maramb";
-            String password = "lepetitcheval";
+        String nomUtilisateur = ((EditText)findViewById(R.id.connexion_nomUtilisateur)).getText().toString();
+        String mdp = ((EditText)findViewById(R.id.connexion_mdp)).getText().toString();
+        String query = "SELECT COUNT(*) FROM users WHERE id = ? AND pwd = ?;";
+        new ExistanceQueryDatabaseAsyncTask().execute(new String[]{query, nomUtilisateur, BCrypt.hashpw(mdp, BCrypt.gensalt())});
+    }
 
-            Connection con = DriverManager.getConnection(url, user, password);
-            System.out.println("Connection effective");
-            String query = "SELECT COUNT(*) FROM users WHERE id = ? AND pwd = ?;";
+    /**
+     * Cette sous-classe a été construite pour réaliser des appels asynchrones en base de donnée pour vérfier l'existence ou non d'un champ.
+     * Elle hérite de la classe AsyncTask de manière à pouvoir s'exécuter en parallèle du Thread principal.
+     * Par défaut de temps, je n'ai pas pu me renseigner suffisamment, et donc la classe n'est pas encore vraiment générique, j'ai dû inclure des instructions propres au problème de connexion de compte.
+     */
+    public class ExistanceQueryDatabaseAsyncTask extends AsyncTask<String,Void,Boolean> {
+        /**
+         * Cette méthode va réaliser en fond le travail de checker en base de donnée l'existence du compte.
+         * @param queryParams   : Paramètres de la requête.
+         * @return              : Renvoie un booléen témoignant de si le test est un succès ou non.
+         */
+        @Override
+        protected Boolean doInBackground(String... queryParams) {
+            try{
+                Class.forName("org.postgresql.Driver");
+                String url = "jdbc:postgresql://ser-info-03.ec-nantes.fr:5432/maramb";
+                String user = "maramb";
+                String password = "lepetitcheval";
+                Connection con = DriverManager.getConnection(url, user, password);
+                String query = queryParams[0];
 
-            PreparedStatement stmt = con.prepareStatement(query);
-            stmt.setString(1, nomUtilisateur);
-            stmt.setString(2, mdp);
-
-            ResultSet rs = stmt.executeQuery();
-            rs.next();
-            int resultat = rs.getInt(1);
-
-            con.close();
-            System.out.println("Connection fermée");
-
-            if (resultat !=0) {
-                Intent intent = new Intent(this, FirstActivity.class);
-                startActivity(intent);
+                PreparedStatement stmt = con.prepareStatement(query);
+                for(int i=0;i<queryParams.length-1;i++){
+                    try{
+                        stmt.setInt(i+1,Integer.parseInt(queryParams[i+1]));
+                    }
+                    catch(Exception e){
+                        stmt.setString(i+1, queryParams[i+1]);
+                    }
+                }
+                ResultSet result = stmt.executeQuery();
+                result.next();
+                if (result.getInt(1)!=0) {
+                    Intent intent = new Intent(ConnexionActivity.this, FirstActivity.class);
+                    Snackbar.make(findViewById(R.id.connexion_layout),R.string.connexion_msg_succes, Snackbar.LENGTH_SHORT).addCallback(new Snackbar.Callback(){
+                        @Override
+                        public void onDismissed(Snackbar snackbar, int event){
+                            startActivity(intent);
+                        }
+                    }).show();
+                }
+                else {
+                    Snackbar.make(findViewById(R.id.connexion_layout),R.string.connexion_msg_echec, Snackbar.LENGTH_SHORT).addCallback(new Snackbar.Callback(){
+                        @Override
+                        public void onDismissed(Snackbar snackbar, int event){
+                            ((EditText)findViewById(R.id.connexion_nomUtilisateur)).setText("");
+                            ((EditText)findViewById(R.id.connexion_mdp)).setText("");
+                        }
+                    }).show();
+                }
+                con.close();
+                return true;
             }
-            else {
-                Intent intent = new Intent(this, CreerCompteActivity.class);
-                startActivity(intent);
+            catch (Exception ex){
+                ex.printStackTrace();
             }
+            return false;
         }
 
-        catch (Exception e){
-            e.printStackTrace();
-            conn_id.setText("Toz");
+        /**
+         * Cette méthode s'occupe d'appliquer les résultats en fonction de l'existence ou non du compte
+         * @param bool  : Réponse de la méthode doInBackground. Indique si le compte existe ou pas.
+         */
+        @Override
+        protected void onPostExecute(Boolean bool) {
+            if(bool){
+                Intent intent = new Intent(ConnexionActivity.this, FirstActivity.class);
+                Snackbar.make(findViewById(R.id.connexion_layout),R.string.connexion_msg_succes, Snackbar.LENGTH_SHORT).addCallback(new Snackbar.Callback(){
+                    @Override
+                    public void onDismissed(Snackbar snackbar, int event){
+                        startActivity(intent);
+                    }
+                }).show();
+            }
+            else{
+                Snackbar.make(findViewById(R.id.connexion_layout),R.string.connexion_msg_echec, Snackbar.LENGTH_SHORT).show();
+                ((EditText)findViewById(R.id.connexion_nomUtilisateur)).setText("");
+                ((EditText)findViewById(R.id.connexion_mdp)).setText("");
+            }
         }
     }
 }
